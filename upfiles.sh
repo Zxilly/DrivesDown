5004e2036ee64efd036eabf7e8cf8f0c8cb92a4f
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2021-02-24 15:24:39 +0800

    Suzumiya Haruhi no Taisei / 凉宫春日的大成 87.8

--- upfiles.sh
+++ upfiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-wget https://nyaa.si/download/1347215.torrent
-aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
-#for ((i=1, j=20; i<=396; i=i+20, j=j+20))
-#do
-#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
+wget https://nyaa.si/download/1183669.torrent
+#aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
+for ((i=1, j=20; i<=1072; i=i+20, j=j+20))
+do
+aria2c -x 64 --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 --select-file=$i-$j *.torrent
 rclone move -v --transfers 8 --checkers 12 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ nice:/视频/
-#echo "$i-$j 上传完成"
-#done
+echo "$i-$j 上传完成"
+done
